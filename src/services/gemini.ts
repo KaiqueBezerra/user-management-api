0340e41112e32222a89532e493b90b6156f69fdf
Kaique <kaiquebezerra30@gmail.com>
2026-04-30 20:14:34 -0300

    chore: update gemini service

--- src/services/gemini.ts
+++ src/services/gemini.ts
@@ -3,7 +3,7 @@ import { GoogleGenAI } from "@google/genai";
 import { env } from "../env.ts";
 
 const gemini = new GoogleGenAI({
-  apiKey: env.GEMINI_API_KEY,
+  apiKey: env.GEMINI_API_KEY || "",
 });
 
 const model = "gemini-2.5-flash";
