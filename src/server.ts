94a374e789c1f3132992767ca9cc11a3e3a86fc4
Kaique <kaiquebezerra30@gmail.com>
2026-04-30 22:20:48 -0300

    chore: change env importantion

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import { fastify } from "fastify";
-import { env } from "./env.ts";
+import { env } from "./env.js";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
