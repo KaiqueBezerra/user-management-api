c2fc9aec6bfe3f60283629b7b40c8a619f320a5b
Kaique <kaiquebezerra30@gmail.com>
2026-04-30 22:13:56 -0300

    chore: add console to test

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
-import fastify from "fastify";
+import { fastify } from "fastify";
 import { env } from "./env.ts";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+console.log("ENV:", process.env);
 
 app.get("/api/health", async () => "OK");
 
